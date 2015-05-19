27cb9f717f7eafd184e42b34a23849b8a57bda2f
Pato <plankenau@gmail.com>
2015-05-19 18:42:59 -0500

    added led function for controlling individual leds, plus a tester function

--- launchpad/launchpad.go
+++ launchpad/launchpad.go
@@ -5,6 +5,7 @@ import (
 	"github.com/rakyll/portmidi"
 	"log"
 	"strings"
+	"time"
 )
 
 type Launchpad struct {
@@ -14,11 +15,37 @@ type Launchpad struct {
 	inputStream  *portmidi.Stream
 }
 
-func main() {
+func snake(launchpad *Launchpad) {
+	var r, g int
+	for on := 0; ; on++ {
+		if on%2 == 0 {
+			r = 3
+			g = 0
+		} else {
+			r = 0
+			g = 3
+		}
+		for x := 0; x <= 8; x++ {
+			for y := 0; y <= 8; y++ {
+				if x%2 == 0 {
+					launchpad.Led(x, y, r, g)
+				} else {
+					launchpad.Led(x, 8-y, r, g)
+				}
+				time.Sleep(30 * time.Millisecond)
+			}
+		}
+	}
+}
 
+func main() {
 	fmt.Println("Initalized")
 
 	launchpad := GetLaunchPad()
+	launchpad.Reset()
+
+	snake(launchpad)
+
 	defer launchpad.Close()
 }
 
@@ -37,9 +64,22 @@ func (launchpad *Launchpad) Close() {
 	launchpad.inputStream.Close()
 	launchpad.outputStream.Close()
 	portmidi.Terminate()
+	fmt.Println("Closed")
 }
 
 func (launchpad *Launchpad) Reset() {
+	launchpad.outputStream.WriteShort(0xb0, 0, 0)
+}
+
+func (launchpad *Launchpad) Led(x, y, r, g int) {
+	vel := 16*g + r + 8 + 4
+	if y == 8 && x != 8 {
+		note := 104 + x
+		launchpad.outputStream.WriteShort(0xb0, int64(note), int64(vel))
+		return
+	}
+	note := x + 16*(7-y)
+	launchpad.outputStream.WriteShort(0x90, int64(note), int64(vel))
 }
 
 func handleError(err error) {
